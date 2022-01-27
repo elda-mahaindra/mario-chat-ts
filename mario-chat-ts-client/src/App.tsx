fdebe952ac2f5aa856556fb25c5778bf6da03220
elda-mahaindra <faith030@gmail.com>
2022-01-28 01:20:36 +0700

    fe - create react app, install dependencies

---
+++ mario-chat-ts-client/src/App.tsx
@@ -0,0 +1,8 @@
+// ---------------------------------------------- modules import
+import { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+  return <div className="App">This will be the app.</div>;
+};
+
+export default App;
